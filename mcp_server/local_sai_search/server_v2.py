4fb7c8e698e19e1133fb10ffca1e7a7fd3683028
xiyuanyang-code <xiyuan__yang@outlook.com>
2025-12-13 20:57:08 +0800

    feat: update local sai mcp server

--- mcp_server/local_sai_search/server_v2.py
+++ mcp_server/local_sai_search/server_v2.py
@@ -4,11 +4,13 @@ import sys
 import logging
 import httpx
 import asyncio
-from mcp.server.fastmcp import FastMCP
-from dotenv import load_dotenv
+import uuid
+import json
+import requests
+import dotenv
 
-# 加载环境变量 (如果 FastMCP 需要 key)
-load_dotenv()
+dotenv.load_dotenv(override=True)
+from mcp.server.fastmcp import FastMCP
 
 sys.path.append(os.getcwd())
 from utils.log_config import setup_logging
@@ -18,6 +20,45 @@ logger = logging.getLogger("MCP_Server")
 mcp = FastMCP("local-rag-database")
 PORT = 39255
 RAG_SERVICE_URL = f"http://127.0.0.1:{PORT}/search"
+
+
+def query_memory(query: str, conversation_id: str = None):
+    if not conversation_id:
+        conversation_id = str(uuid.uuid4())
+
+    data = {
+        "query": query,
+        "user_id": "memos_user_geekcenter",
+        "conversation_id": conversation_id,
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Token {os.environ['MEMOS_API_KEY']}",
+    }
+    url = f"{os.environ['MEMOS_BASE_URL']}/search/memory"
+    res = requests.post(url=url, headers=headers, data=json.dumps(data))
+    return res.json()
+
+
+@mcp.tool()
+async def local_search_enhanced(query: str):
+    """上海交通大学人工智能学院 (SJTU AI) 本地数据库检索工具（云端增强版）
+
+    调用条件: 当用户的提问 (query) 明确涉及上海交通大学人工智能学院 (SJTU AI) 的课程、科研、师资、培养方案、招生、学生生活、政策或其他相关内部知识时，应当调用此工具。
+    功能: 在本地知识库中检索与 query 最相关的信息。
+    如果你调用 local_search 失败 你不妨调用这个试一试！
+
+    Args:
+        query (str): 用户的原始问题或检索请求。
+
+    Returns:
+        dict: 检索结果列表。
+    """
+    try:
+        res = query_memory(query=query)
+        return res
+    except Exception as e:
+        return f"Error, Calling Local Search Enhanced Failed: {e}"
 
 
 @mcp.tool()
