608c0463171c04caaf1c1f5d5c90ad31d1d06f8a
xiyuanyang-code <xiyuan__yang@outlook.com>
2026-02-20 13:18:19 +0800

    fix: remove incorrect backends folder

--- backends/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Backends layer for IntelliSearch UI adaptation.
-
-This layer provides UI-specific implementations that connect the Service layer
-to concrete UI frameworks (CLI, Web, etc.).
-"""
